e21ed1b2f60bb6ebe617a9e72758848d808d26ff
dbha <dbha0719@gmail.com>
2022-11-06 16:19:42 +0900

    update

---
+++ kp_create_image.sh
@@ -0,0 +1,8 @@
+kp secret create harborapp-registry-creds --registry harborapp.shared.lab.pksdemo.net --registry-user admin
+
+kp image save sample-convention-server \
+  --tag harborapp.shared.lab.pksdemo.net/library/sample-convention-server \
+  --git https://github.com/dbha/sample-convention-server.git \
+  --git-revision main \
+  --env BP_JVM_VERSION=17 \
+  --wait
